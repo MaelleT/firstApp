8bb4d5ea9066613b0022158eafe74f97af288e60
MaelleT <maelle.taurand@ac-nantes.fr>
2016-05-30 21:46:49 +0200

    Models administrable depuis interface admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question,Choice
 
 # Register your models here.
+admin.site.register(Question) # permet d'administrer le model Question depuis l'interface admin
+admin.site.register(Choice)
