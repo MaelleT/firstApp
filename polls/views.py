37dac854059741f70eecb7cb1e4495bbb6a69f5c
MaelleT <maelle.taurand@ac-nantes.fr>
2016-05-30 22:24:48 +0200

    Vue index avec un template

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,30 @@
 #from django.shortcuts import render
 from django.http.response import HttpResponse
-
+from django.template import loader
+from .models import Question
+# for each view : all django wants is that HttpResponse. Or an exception.
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    
+    #use template
+    template = loader.get_template('polls/index.html')
+    context={
+             'latest_question_list':latest_question_list
+             }
+    
+    #sans le template
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    
+    return HttpResponse(template.render(context,request))
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at question %s" % question_id)
+
+def results(request,question_id):
+    return HttpResponse("You are looking at the results of question %s" % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
