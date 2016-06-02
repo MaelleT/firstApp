74dd5ef3788a97f4665a9be0564d55fb6d3e0542
MaelleT <maelle.taurand@ac-nantes.fr>
2016-06-02 20:37:48 +0200

    generic views

--- polls/views.py
+++ polls/views.py
@@ -5,16 +5,28 @@ from django.http import Http404
 from django.template import loader
 from .models import Question
 
+from django.views import generic
+
 from django.shortcuts import render, get_object_or_404
 # for each view : all django wants is that HttpResponse. Or an exception.
 from .models import Choice,Question
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
+from datetime import timezone
+
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name ='latest_question_list'
     
-    context={'latest_question_list':latest_question_list}
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
     
-    return render(request,'polls/index.html',context)
+    #Before generic view
+    #latest_question_list = Question.objects.order_by('pub_date')[:5]
+    
+    #context={'latest_question_list':latest_question_list}
+    
+    #return render(request,'polls/index.html',context)
   
     #use template without shortcut
     #template = loader.get_template('polls/index.html')
@@ -27,21 +39,37 @@ def index(request):
     
 
 
-def detail(request,question_id):
-    try :
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+class TheDetailView(generic.DetailView):
+#def detail(request,question_id):   
+    model=Question
+    template_name='polls/detail.html'
+    
+    def get_object(self):
+        return Question.objects.get(pk=self.kwargs.get("question_id"))
+    
+    #before generic view
+    #try :
+    #    question=Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
 
     #le bloc try except peut être remplacé par get_object_or_404(Question,pk=question_id)
     
-    return render(request,'polls/detail.html',{'question':question})
+    #return render(request,'polls/detail.html',{'question':question})
 
-
-def results(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
+class ResultsView(generic.DetailView):
+#def results(request,question_id):
     
-    return render(request,'polls/results.html',{'question':question})
+    model=Question
+    template_name='polls/results.html'
+    
+    def get_object(self):
+        return Question.objects.get(pk=self.kwargs.get("question_id"))
+    
+    #before generic view
+    #question = get_object_or_404(Question, pk=question_id)
+    
+    #return render(request,'polls/results.html',{'question':question})
     #return HttpResponse("You are looking at the results of question %s" % question_id)
 
 def vote(request,question_id):
