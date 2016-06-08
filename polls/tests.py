3d00acac528919b9cf67137e9d9cd81781de4c48
MaelleT <maelle.taurand@ac-nantes.fr>
2016-06-08 20:28:43 +0200

    Unittest for model method was_published

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from .models import Question
 from django.test import TestCase
 
-class QuestionMethodTests(TestCase):
+class TestQuestionMethodFutur(TestCase):
 
     def setUp(self):
         TestCase.setUp(self)
@@ -21,3 +21,33 @@ class QuestionMethodTests(TestCase):
     def test_was_published_with_future_question(self):
        
         self.assertEqual(self.future_question.was_published(), False,"Erreur : question with future pub_date")
+        
+class TestQuestionMethodOld(TestCase):
+
+    def setUp(self):
+        TestCase.setUp(self)
+        time = timezone.now()- datetime.timedelta(days=30)
+        self.old_question=Question(pub_date=time)
+        
+    def tearDown(self):
+        TestCase.tearDown(self)
+        
+    def test_was_published_with_old_question(self):
+       
+        self.assertEqual(self.old_question.was_published(), False,"Erreur : question with old pub_date")
+        
+class TestQuestionMethodRecent(TestCase):
+
+    def setUp(self):
+        TestCase.setUp(self)
+        time = timezone.now()- datetime.timedelta(hours=1)
+        self.recent_question=Question(pub_date=time)
+        
+    def tearDown(self):
+        TestCase.tearDown(self)
+        
+    def test_was_published_with_recent_question(self):
+       
+        self.assertEqual(self.recent_question.was_published(), True,"Erreur : question with recent pub_date")
+        
+        
