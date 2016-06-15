f790bedf300f2f233d540ff2f23b8fe5f7b7e092
MaelleT <maelle.taurand@ac-nantes.fr>
2016-06-15 20:59:39 +0200

    how to create a  model form

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import SignUp
+from .forms import SignUpForm
 
 
 class SignUpAdmin(admin.ModelAdmin):
     #permet de personnaliser l'interface admin
     list_display = ["__str__","timestamp"]
-    class Meta :
-        model = SignUp
+    form = SignUpForm
+    
+    #before create forms : SignUpForm
+    #class Meta :
+    #    model = SignUp
     
 
 admin.site.register(SignUp,SignUpAdmin)
