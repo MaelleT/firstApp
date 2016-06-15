1c6a0ae88b96eabde0846e9d6b79809a24c4845b
MaelleT <maelle.taurand@ac-nantes.fr>
2016-06-15 21:47:36 +0200

    communication with context request beetween views and template

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import request
 
-# Create your views here.
+# Create yoursviews here.
 def home(request):
-    return render(request,"home.html",{})
+    title = "welcome"
+    if request.user.is_authenticated():
+        title = "Welcome %s" %(request.user.username)
+    
+    #add a form
+    context = {
+                   "title" : title,
+                  } 
+    return render(request,"home.html",context)
+
