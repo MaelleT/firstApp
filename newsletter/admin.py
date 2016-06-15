a0c8f2fcf47c5ef31199b66b472155e988bdb119
MaelleT <maelle.taurand@ac-nantes.fr>
2016-06-15 20:44:40 +0200

    Personnaliser l'interface admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+
+class SignUpAdmin(admin.ModelAdmin):
+    #permet de personnaliser l'interface admin
+    list_display = ["__str__","timestamp"]
+    class Meta :
+        model = SignUp
+    
+
+admin.site.register(SignUp,SignUpAdmin)
