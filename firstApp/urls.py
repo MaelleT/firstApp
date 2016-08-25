1a3e09d99ad4e6eaeeee165810406b29daa4a5d7
MaelleT <maelle.taurand@ac-nantes.fr>
2016-08-25 19:36:22 +0200

    test connexion Mantis

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+import newsletter.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    #url(r'^newsletter/home/$',newsletter.views.home,name='home'),
+
     url(r'^newsletter/',include('newsletter.urls'))
 ]
